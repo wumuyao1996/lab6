1a1853a7bf36e8ade9f3ca56da3ff4027471f333
vagrant <vagrant@precise32.(none)>
2015-02-13 19:18:16 +0000

    putting it together lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,13 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" +idNumber, logme);
+
+
+
+
+  //$('div.details').text("We Do Not Sow -- Words of House Greyjoy");
+
 }
 
 /*
@@ -35,4 +42,47 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", coloring);
 }
+
+/*
+ * get jfunctuon
+ */
+
+function coloring(thecolor)
+{
+	console.log(thecolor);
+
+	var colors = thecolor["hex"];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+function logme(result) {
+
+	console.log(result);
+	var proto = "#project" + result['id'];
+	$(proto + " .details").html("<h3>" + result['date'] + "</h3>" + result['summary']);
+
+	var projectHTML = '<a href = "#" class= "thumbnail">' +
+		'<img src= "' + result['image'] + '" class = "img">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' +result['date'] + '</small></p></a>';
+
+		$("#project-container").html(projectHTML);
+		$("#project-description").html(result['summary']);
+}
+
+
+
+
+
+
+
+
+
+
